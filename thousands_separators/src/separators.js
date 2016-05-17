a4e2f155cf5e2945738781abbd867161f16dbc4f
Fiooona <845559655@qq.com>
2016-05-17 10:31:45 +0800

    2

--- thousands_separators/src/separators.js
+++ thousands_separators/src/separators.js
@@ -1,13 +1,14 @@
 'use strict';
 
 function thousands_separators(num) {
-    var s=parseFloat(num);
-    if(s.indexOf(".")==-1) s+=".0";
-    while(/\d{4}(\.|,)/.test(s)){
-        s= s.replace(/(\d)(\d{3}(\.|,))/,"$1,$2");
-        return s;
+    num=num.toString();
+    if(num.indexOf(".")==-1) num+=".0";
+   var num1=num.split(".");
+    while(/\d{4}/.test(num1[0])){
+        //如果有4个相连的数字
+        num1[0]=num.replace(/(\d+)(\d{3})/,'$1,$2');
     }
+    num=num1[0]+"."+num2[1];
 }
-
 module.exports = thousands_separators;
 
